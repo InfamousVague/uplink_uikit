33cbe7b5535f7d415e9e7683d74facd491b333fe
lgmarchi <lucasgmarchi@gmail.com>
2024-04-17 14:33:47 -0300

    Finish merge from dev

--- ui/src/layouts/log_in/copy_seed_words.rs
+++ ui/src/layouts/log_in/copy_seed_words.rs
@@ -22,45 +22,46 @@ use warp::multipass;
 // styles for this layout are in layouts/style.scss
 #[component]
 pub fn Layout(page: Signal<AuthPages>, username: String, pin: String) -> Element {
-    let state = use_ref(cx, State::load);
+    let state = use_signal(State::load);
     let window = use_window();
+    let words: Signal<Option<(String, Vec<String>)>> = use_signal(|| None);
 
-    if !matches!(&*page.current(), AuthPages::Success(_)) {
+    if !matches!(&*page.read(), AuthPages::Success(_)) {
         window.set_inner_size(LogicalSize {
             width: 500.0,
             height: 480.0,
         });
     }
 
-    let words = use_future(move || async move {
+    use_resource(move || async move {
         let mnemonic = warp::crypto::keypair::generate_mnemonic_phrase(
             warp::crypto::keypair::PhraseType::Standard,
         )
         .into_phrase();
-        (
+        (words.set(Some((
             mnemonic.clone(),
             mnemonic
                 .split_ascii_whitespace()
                 .map(|x| x.to_string())
                 .collect::<Vec<String>>(),
-        )
+        ))),)
     });
 
     rsx!(
-        style {get_app_style(&state.read())},
+        style {{get_app_style(&state.read())}},
         div {
             id: "copy-seed-words-layout",
             aria_label: "copy-seed-words-layout",
             div {
                 class: "instructions-important",
-                get_local_text("copy-seed-words.instructions")
+                {get_local_text("copy-seed-words.instructions")}
             },
             Label {
-                aria_label: "copy-seed-words".into(),
+                aria_label: "copy-seed-words".to_string(),
                 text: get_local_text("copy-seed-words")
             },
-            if let Some((seed_words, words)) = words.value() {
-                rsx!{ {SeedWords { page: page.clone(), username: username.clone(), pin: pin.clone(), seed_words: seed_words.clone(), words: words.clone() }} }
+            if let Some((seed_words, words)) = words() {
+                {rsx!(SeedWords { page: page.clone(), username: username.clone(), pin: pin.clone(), seed_words: seed_words.clone(), words: words.clone() })}
             }
         }
     )
@@ -127,40 +128,44 @@ fn SeedWords(
             },
         ))},
         div {
-            class: format_args!("seed-words {}", if *loading.get() {"progress"} else {""}),
-            words.chunks_exact(2).enumerate().map(|(idx, vals)| rsx! {
+            class: format_args!("seed-words {}", if loading() {"progress"} else {""}),
+            {words.chunks_exact(2).enumerate().map(|(idx, vals)| rsx! {
                 div {
                     class: "row",
                     div {
                         class: "col",
                         span {
                             aria_label: "seed-word-number-{((idx * 2) + 1).to_string()}",
-                            class: "num disable-select", ((idx * 2) + 1).to_string()
+                            class: "num disable-select",
+                            {((idx * 2) + 1).to_string()}
                         },
                         span {
                             aria_label: "seed-word-value-{((idx * 2) + 1).to_string()}",
-                            class: "val", vals.first().cloned().unwrap_or_default()
+                            class: "val",
+                            {vals.first().cloned().unwrap_or_default()}
                         }
                     },
                     div {
                         class: "col",
                         span {
                             aria_label: "seed-word-number-{((idx * 2) + 2).to_string()}",
-                            class: "num disable-select", ((idx * 2) + 2).to_string()
+                            class: "num disable-select",
+                            {((idx * 2) + 2).to_string()}
                         },
                         span {
                             aria_label: "seed-word-value-{((idx * 2) + 2).to_string()}",
-                            class: "val", vals.get(1).cloned().unwrap_or_default()
+                            class: "val",
+                            {vals.get(1).cloned().unwrap_or_default()}
                         }
                     }
                 }
-            })
+            })}
         },
         div {
             class: "controls",
             Button {
                 text: get_local_text("uplink.copy-seed"),
-                aria_label: "copy-seed-button".into(),
+                aria_label: "copy-seed-button".to_string(),
                 icon: icons::outline::Shape::BookmarkSquare,
                 onpress: move |_| {
                     match Clipboard::new() {
@@ -182,7 +187,7 @@ fn SeedWords(
             class: "controls",
             Button {
                 text: get_local_text("uplink.go-back"),
-                disabled: *loading.get(),
+                disabled: loading(),
                 aria_label: "back-button".into(),
                 icon: icons::outline::Shape::ChevronLeft,
                 onpress: move |_| page.set(AuthPages::CreateOrRecover),
@@ -190,8 +195,8 @@ fn SeedWords(
             },
             Button {
                 aria_label: "i-saved-it-button".into(),
-                disabled: *loading.get(),
-                loading: *loading.get(),
+                disabled: loading(),
+                loading: loading(),
                 text: get_local_text("copy-seed-words.finished"),
                 onpress: move |_| {
                     ch.send(());
@@ -201,7 +206,7 @@ fn SeedWords(
         {copied.read().then(||{
             rsx!(div{
                 class: "copied-toast",
-                get_local_text("uplink.copied-seed")
+                {get_local_text("uplink.copied-seed")}
             })
         })}
     }
